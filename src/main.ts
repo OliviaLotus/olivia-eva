29d0d5950d394f3a56e2105260b499611257866c
OliviaLotus <qianseyear@gmail.com>
2025-11-27 13:55:08 +0800

    添加 NProgress 进度条配置和 VxeTable 全局配置，重构 main.ts 以集成新功能

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-// ===== 全局组件 =====
 import * as ElementPlusIcons from '@element-plus/icons-vue'
 import { createApp } from 'vue'
-
-// ===== 核心配置 =====
+import VXETable from 'vxe-table'
+import { setupWebSocket } from '@/composables'
 import { setupDirective } from '@/directives'
 import { setupI18n } from '@/lang'
+import { configureVxeTable } from '@/plugins/vxe-table'
 import { setupRouter } from '@/router'
+import { setupPermissionGuard } from '@/router/guards/permission'
 import { setupStore } from '@/store'
 import App from './App.vue'
+
 // ===== 样式导入 =====
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
@@ -15,6 +17,7 @@ import 'vxe-table/lib/style.css'
 import '@/styles/index.scss'
 import 'uno.css'
 import 'animate.css'
+
 // 创建 Vue 应用实例
 const app = createApp(App)
 
@@ -28,6 +31,16 @@ setupI18n(app)
 Object.entries(ElementPlusIcons).forEach(([name, comp]) =>
   app.component(name, comp),
 )
+
+// 3.第三方插件
+configureVxeTable()
+app.use(VXETable)
+
+// 4.路由守卫
+setupPermissionGuard()
+
+// 5.WebSocket 初始化
+setupWebSocket()
 
 // 6.挂载应用
 app.mount('#app')
